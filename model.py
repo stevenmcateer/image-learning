8463dd445843415cb7b58e92c2f42aa2501687ca
Steven McAteer <smcateer@wpi.edu>
2019-04-09 15:46:52 -0400

    broke into functions

--- model.py
+++ model.py
@@ -1,7 +1,37 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
-import tensorflow as tf
 import numpy as np
+
+
+def main():
+    # Initialize weights randomly for each layer
+    np.random.seed(0)
+    images = preprocess_images(np.load("images.npy"))
+    labels = preprocess_labels(np.load("labels.npy"))
+
+
+def preprocess_images(images):
+    # Flatten the matrices to 1x784 vectors
+    flat_images = []
+    for i in range(len(images)):
+        flat_images.append(images[i].flatten())
+
+    images = np.array(flat_images)
+    # images = images.astype('float32')
+    # images /= 255
+    return images
+
+def preprocess_labels(labels):
+    return np_utils.to_categorical(labels, 10)
+
+# def split_data():
+#     # Training set has 60%
+#
+#
+#     # Validation set has 15%
+#
+#     # Test set has 25%
+
 
 # Model Template
 
@@ -9,8 +39,6 @@ model = Sequential() # declare model
 model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
 model.add(Activation('relu'))
 #
-#
-# Initialize weights randomly for each layer
 # Experiment with ReLu Activation Units, as well as SeLu and Tanh
 # Experiment with number of layers/num of neurons per layer
 #
@@ -38,3 +66,7 @@ history = model.fit(x_train, y_train,
 
 print(history.history)
 model.predict()
+
+
+if __name__ == '__main__':
+    main()
